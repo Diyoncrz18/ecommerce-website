ae9e427f032a91a51ff03a39a457ce6406f6bdb3
Diyoncrz18 <dkobi16@gmail.com>
2025-02-20 01:14:18 +0800

    first commit admin-pages

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "../../components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { UserProfile } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <UserButton />
+    </div>
+  );
+}
